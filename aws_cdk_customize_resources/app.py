b67884f9f9b845aec8110b9e630ddcd5ba64797a
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2021-09-13 15:49:17 +1000

    AWS CDK Secrets & SSM Parameter

--- aws_cdk_customize_resources/app.py
+++ aws_cdk_customize_resources/app.py
@@ -13,22 +13,9 @@ from aws_cdk_customize_resources.aws_cdk_customize_resources_stack import AwsCdk
 
 
 app = core.App()
-AwsCdkCustomizeResourcesStack(app, "AwsCdkCustomizeResourcesStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=core.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+#import environment
+env_dev= core.Environment(account="673569942958", region="ap-southeast-2")
+AwsCdkCustomizeResourcesStack(app, "AwsCdkCustomizeResourcesStack" , env=env_dev )
 
 app.synth()
--- aws_cdk_customize_resources/aws_cdk_customize_resources/aws_cdk_customize_resources_stack.py
+++ aws_cdk_customize_resources/aws_cdk_customize_resources/aws_cdk_customize_resources_stack.py
@@ -1,10 +1,11 @@
 from aws_cdk import core as cdk
+import json
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core,
+    aws_ssm as _ssm,
+    aws_secretsmanager as _secretsmanager
+)
 
 
 class AwsCdkCustomizeResourcesStack(cdk.Stack):
@@ -13,3 +14,71 @@ class AwsCdkCustomizeResourcesStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        #Create AWS SSM parameter
+        parameter1 = _ssm.StringParameter(
+            self,
+            "parameter1",
+            description = "Load Testing Configuration",
+            parameter_name = "NoOfConcurrentUsers",
+            string_value = "100",
+            tier = _ssm.ParameterTier.STANDARD
+
+        )
+        parameter2 = _ssm.StringParameter(
+            self,
+            "parameter2",
+            description = "Load Testing Configuration",
+            parameter_name = "/local/config/huifolder",
+            string_value = "100",
+            tier = _ssm.ParameterTier.STANDARD
+
+        )
+
+        parameter3 = _ssm.StringParameter(
+            self,
+            "parameter3",
+            description = "Load Testing Configuration",
+            parameter_name = "/local/config/huifolder2",
+            string_value = "300",
+            tier = _ssm.ParameterTier.STANDARD
+
+        )
+
+        #Create secret manager
+        secret1 = _secretsmanager.Secret(
+            self,
+            "secret1",
+            description = "Customer DB Password",
+            secret_name = "my_db_password"
+        )
+
+        #templated secret
+        templated_secret = _secretsmanager.Secret(
+            self,
+            "secret2",
+            description="A templated secret for user data",
+            secret_name = "user_kon_attributes",
+            generate_secret_string = _secretsmanager.SecretStringGenerator(
+                secret_string_template = json.dumps(
+                    {"username": "kon"}
+                ),
+                generate_string_key = "password"
+            )
+        )
+
+
+
+        output_Param1 = core.CfnOutput(
+            self,
+           "param1",
+           description="NoOfConcurrentUser",
+           value=f"{parameter1.string_value}" 
+        )
+
+        output_Param2 = core.CfnOutput(
+            self,
+            "secretValue1",
+            description = "secret1",
+            value= f"{secret1.secret_value}"
+        )
